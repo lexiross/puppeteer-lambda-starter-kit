9d12bdb423a6be3b27a7677ff2d500f6f87c7bcc
Lexi Ross <lexi@aspen.team>
2018-03-04 14:46:42 -0800

    copy in wodify logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const setup = require('./starter-kit/setup');
+const fetchWorkout = require('./fetchWorkout');
 
 exports.handler = async (event, context, callback) => {
-  // For keeping the browser launch
-  context.callbackWaitsForEmptyEventLoop = false;
-  const browser = await setup.getBrowser();
-  exports.run(browser).then(
-    (result) => callback(null, result)
-  ).catch(
-    (err) => callback(err)
-  );
+    // For keeping the browser launch
+    context.callbackWaitsForEmptyEventLoop = false;
+    const browser = await setup.getBrowser();
+    exports.run(browser).then(
+        (result) => callback(null, result)
+    ).catch(
+        (err) => callback(err)
+    );
 };
 
 exports.run = async (browser) => {
-  const page = await browser.newPage();
-  await page.goto('https://www.google.co.jp');
-  await page.close();
-  return 'done';
+    const workout = await fetchWorkout();
+    return workout;
 };
